a679e69e6d0fcf14115cc83e39b46b515bb0a07f
Janmie-CJM <392740909@qq.com>
2022-12-16 17:47:14 +0800

    update

---
+++ src/components/Sheet.vue
@@ -0,0 +1,74 @@
+<template>
+	<div class="container-fluid">
+		<div class="row">
+			<main role="main" class="col-md-12 ml-sm-auto col-lg-12 pt-3 px-4">
+				<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 ">
+					<h2>VueSheet List</h2>
+					<div class="btn-toolbar mb-2 mb-md-0">
+						<a 	:href="`https://docs.google.com/spreadsheets/d/${excel_file_id}/edit?usp=sharing`"
+							class="btn btn-sm btn-outline-secondary"
+							target="_blank">
+						View Google Sheet
+						</a>
+					</div>
+				</div>
+				<div class="table-responsive">
+				<table class="table table-striped ">
+					<thead>
+					<tr>
+						<th>Name</th>
+						<th>Phone</th>
+						<th>Activity</th>
+						<th>City</th>
+					</tr>
+					</thead>
+					
+					<tbody>
+						<Row v-bind:key="row.id" v-for="row in rows" v-bind:row="row" />
+					</tbody>
+				</table>
+				</div>
+			</main>
+		</div>
+	</div>
+</template>
+ 
+<script>
+import Row from '@/components/Row.vue';
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const creds = require('@/client_secret.json');
+	export default {
+		name: "Sheet",
+		components: {
+			Row
+		},
+		props: ["sheet"],
+		data() {
+			return {
+				rows: [],
+				loading: true,
+				excel_file_id :['1oLGI1u68sPh-P1yhWaU7j_WUTHjFMY1aUu6uINfTb0Q','1JO54FSyB-TGoFp8NJyw0PAwqIUxkF7wukWNRmBxVyX0'][1]
+			}
+		},
+		methods:{
+			async accessSpreadSheet() {
+				const doc = new GoogleSpreadsheet(this.excel_file_id);
+				await doc.useServiceAccountAuth(creds);
+				await doc.loadInfo(); 
+				const sheet = doc.sheetsByIndex[0];
+				const  rows = await sheet.getRows({
+					offset: 1
+				})
+				this.rows = rows;
+			}
+		},
+		created() {
+			this.accessSpreadSheet();
+		}
+		
+	}
+</script>
+
+<style scoped>
+
+</style>
